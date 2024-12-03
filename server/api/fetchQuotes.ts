ce83b5e34a4c79547e8111f11dc2dc8144210470
Wakoz <julien.cartron.pro@gmail.com>
2024-12-03 12:53:30 +0100

    Create API

--- server/api/fetchQuotes.ts
+++ server/api/fetchQuotes.ts
@@ -1,10 +1,10 @@
 import type { quotesInfo } from "./quotesInfo";
-const API_URL = 'http://localhost:3000/api';
+const API_URL = "http://localhost:3000/api";
 
 export const fetchQuotes = async (): Promise<quotesInfo[]> => {
   const response = await fetch(`${API_URL}/quotes`);
   if (!response.ok) {
-    throw new Error('Failed to fetch quotes');
+    throw new Error("Failed to fetch quotes");
   }
   return response.json();
 };
